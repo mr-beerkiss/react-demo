e87298fc1d89f0b58b608edbf43e244ae3041943
Damon Kreft <damon.kreft@operationorange.uk>
2017-01-30 23:36:28 +0000

    Refactored components into what I think is a more sane structure and ensured modules that contain JSX have the .jsx extension (making React components more clear and helps with editors)

---
+++ src/components/App.test.jsx
@@ -0,0 +1,9 @@
+/* eslint-env jest */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+
+it('successful render', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App.jsx'
+import './global.css'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+)
