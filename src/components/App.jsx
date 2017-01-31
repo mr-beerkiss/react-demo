c33cd6e3f83724ab024face295bc1902044a98f5
Damon Kreft <damon.kreft@operationorange.uk>
2017-01-31 00:00:20 +0000

    Removed local scoping of CSS modules as learned it doesn't play well with react-create-app and Jest's handy snapshot testing feature (the webpack generated locally scoped classNames aren't generated) so reverting back to global classes and BEM naming convention

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
-import styles from './App.css'
+import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className={styles['App']}>
-        <div className={styles['App__header']}>
+      <div className='App'>
+        <div className='App__header'>
           <img
             src={logo}
-            className={styles['App__header__logo']}
+            className='App__header__logo'
             alt='logo' />
           <h2>Welcome to Damon's Demo Task List</h2>
         </div>
